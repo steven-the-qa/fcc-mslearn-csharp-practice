1f54f1a94f1886751a54ef84e109547fe9060693
Steven Boutcher <boutchersj@gmail.com>
2024-12-26 14:19:29 -0600

    Add files via upload

---
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int[] sophiaScores = [90, 86, 87, 98, 100];
+int[] andrewScores = [92, 89, 81, 96, 90];
+int[] emmaScores = [90, 85, 87, 98, 68];
+int[] loganScores = [90, 95, 87, 88, 96];
+
+int sophiaSum = 0;
+int andrewSum = 0;
+int emmaSum = 0;
+int loganSum = 0;
+
+decimal sophiaScore;
+decimal andrewScore;
+decimal emmaScore;
+decimal loganScore;
+
+foreach(int score in sophiaScores)
+{
+    sophiaSum += score;
+};
+foreach(int score in andrewScores)
+{
+    andrewSum += score;
+};
+foreach(int score in emmaScores)
+{
+    emmaSum += score;
+};
+foreach(int score in loganScores)
+{
+    loganSum += score;
+}
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+andrewScore = (decimal)andrewSum / currentAssignments;
+emmaScore = (decimal)emmaSum / currentAssignments;
+loganScore = (decimal)loganSum / currentAssignments;
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string message = "The quick brown fox jumps over the lazy dog.";
+
+// Reverse the message
+char[] newMessage = message.ToCharArray();
+Array.Reverse(newMessage);
+
+// Count the o's
+int numberOfOs = 0;
+foreach (char i in message)
+{
+    if (i == 'o')
+    {
+        numberOfOs++;
+    }
+}
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {numberOfOs} times.");
